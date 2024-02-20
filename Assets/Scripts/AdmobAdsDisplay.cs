5ac19eeb835d8c3db355b5d5de027cab784623e2
MD RAFIK TADAVI <mdrafiktadavi27@gmail.com>
2024-02-20 17:28:47 +0530

    First Task ompleted

--- Assets/Scripts/AdmobAdsDisplay.cs
+++ Assets/Scripts/AdmobAdsDisplay.cs
@@ -7,7 +7,7 @@ public class AdmobAdsDisplay : MonoBehaviour
 {
     public string appId = "";
     [SerializeField] private string bannerId = "";
-    [SerializeField] private string interstitialId = "";
+    private string interstitialId = "";
     [SerializeField] private string rewardId = "";
 
     BannerView bannerAd;
@@ -24,6 +24,7 @@ public class AdmobAdsDisplay : MonoBehaviour
 
     }
 
+    //Banner Ads
     public void LoadBannerAd()
     {
         createBanner();
@@ -101,5 +102,79 @@ public class AdmobAdsDisplay : MonoBehaviour
             bannerAd.Destroy();
             bannerAd = null;
         }
+    }
+
+
+    //Rewarded Ads
+    public void LoadRewardedAd()
+    {
+        if(rewardedAd != null)
+        {
+            rewardedAd.Destroy();
+            rewardedAd = null;
+        }
+        var adRequest = new AdRequest();
+        adRequest.Keywords.Add("unity-admob-sample");
+
+        RewardedAd.Load(rewardId, adRequest, (RewardedAd ad, LoadAdError error) =>
+        {
+            if(error != null || ad == null)
+            {
+                print("Rewarded failed to load" + error);
+                return;
+            }
+            print("Rewarded ads load successfully");
+            rewardedAd = ad;
+            RewardedAdEvents(rewardedAd);
+        });
+    }
+    public void ShowRewardedAd()
+    {
+        if(rewardedAd != null && rewardedAd.CanShowAd())
+        {
+            rewardedAd.Show((Reward reward) => {
+                
+            });
+        }
+        else
+        {
+            print("Rewarded ad not ready");
+        }
+    }
+    public void RewardedAdEvents(RewardedAd ad)
+    {
+        // Raised when the ad is estimated to have earned money.
+        ad.OnAdPaid += (AdValue adValue) =>
+        {
+            Debug.Log("Rewarded ad paid {0} {1}." +
+                adValue.Value+
+                adValue.CurrencyCode);
+        };
+        // Raised when an impression is recorded for an ad.
+        ad.OnAdImpressionRecorded += () =>
+        {
+            Debug.Log("Rewarded ad recorded an impression.");
+        };
+        // Raised when a click is recorded for an ad.
+        ad.OnAdClicked += () =>
+        {
+            Debug.Log("Rewarded ad was clicked.");
+        };
+        // Raised when an ad opened full screen content.
+        ad.OnAdFullScreenContentOpened += () =>
+        {
+            Debug.Log("Rewarded ad full screen content opened.");
+        };
+        // Raised when the ad closed full screen content.
+        ad.OnAdFullScreenContentClosed += () =>
+        {
+            Debug.Log("Rewarded ad full screen content closed.");
+        };
+        // Raised when the ad failed to open full screen content.
+        ad.OnAdFullScreenContentFailed += (AdError error) =>
+        {
+            Debug.LogError("Rewarded ad failed to open full screen content " +
+                           "with error : " + error);
+        };
     }
 }
